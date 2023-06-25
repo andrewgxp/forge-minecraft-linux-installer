035428c1446ec36470cffba2663ecc9757780238
andrewgxp <38545191+andrewgxp@users.noreply.github.com>
2023-06-25 13:23:32 -0400

    Update forge-minecraft-linux-installer.sh
    Refixing the server.properties URL and changing installer to be silent

--- forge-minecraft-linux-installer.sh
+++ forge-minecraft-linux-installer.sh
@@ -70,8 +70,7 @@ run-forge-installer() {
         exit 1
     fi
 
-    java -jar forge-installer.jar --installServer
-
+    java -jar forge-installer.jar --installServer >/dev/null 2>&1
 }
 
 add-eula-agreement() {
@@ -93,7 +92,7 @@ download-server-properties() {
 
     echo "Downloading a default server.properties file"
 
-    curl -o server.properties https://raw.githubusercontent.com/itzg/docker-minecraft-server/master/server.properties
+    curl -o server.properties https://github.com/vxgxp/forge-minecraft-linux-installer/blob/main/server.properties
 }
 
 add-run-script-to-systemd() {
